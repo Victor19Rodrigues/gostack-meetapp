bc6b47a8cd610e032a166b843193fba2239f7c30
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2019-10-03 21:00:45 -0300

    + Created store files.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -9,7 +11,11 @@ import validateUserStore from './app/validators/UserStore';
 import validateUserUpdate from './app/validators/UserUpdate';
 import validateSessionStore from './app/validators/SessionStore';
 
+import multerConfig from './config/multer';
+
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.post('/users', validateUserStore, UserController.store);
 routes.post('/sessions', validateSessionStore, SessionController.store);
@@ -17,5 +23,6 @@ routes.post('/sessions', validateSessionStore, SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', validateUserUpdate, UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
